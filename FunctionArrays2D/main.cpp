859f2abba79a7329da5ee4a1e82e1228b0d2c671
Admin <Admin@DESKTOP-8URQBFL>
2025-05-23 21:01:14 +0500

    123

--- FunctionArrays2D/main.cpp
+++ FunctionArrays2D/main.cpp
@@ -1,116 +1,96 @@
-﻿//#include<iostream>
-//using namespace std;
-//
-//void randomArr2D(int arr[], const int Height, const int Wight);
-//
-////void randomArr2D(double arr2D, int ROWS, int COLS, int minRand = 0, int maxRand = 100);
-//
-////void printArr2D(int arr2D, int ROWS, int COLS);
-////void printArr2D(double arr2D, const int ROWS, const int COLS);
-//
-//
-//void main()
-//{
-//	setlocale(LC_ALL, "");
-//	const int ROWS = 5;
-//	const int COLS = 8;
-//	int arr2D[ROWS][COLS];
-//	
-//	randomArr2D(arr2D, ROWS, COLS);
-//	//printArr2D(arr2D, ROWS, COLS);
-//	
-//
-//	//double arr2D[ROWS][COLS];
-//
-//	//randomArr2D(arr2D, ROWS, COLS);
-//	//printArr2D(arr2D[ROWS][COLS]);
-//}
-//
-//void randomArr2D(int arr[], const int Height, const int Wight)
-//{
-//	for (int i = 0; i < Height * Wight; i++)
-//		arr[i] = rand() % 100;
-//}
-//void printArr2D(int arr[], int arr2D[ROWS][COLS], const int Height, const int Wight)
-//{
-//	for (int i = 0; i < Height; i++)
-//	{
-//		int k = 0;
-//		for (int j = 0; j < Wight; j++)
-//		{
-//			arr2D[i][j] = arr[k];
-//			cout << arr[i][j] << "/t";
-//			k++;
-//		}
-//		cout << endl;
-//	}
-//}
-////void randomArr2D(double arr2D, int ROWS, int COLS)
-////{
-////
-////	for (int i = 0; i < ROWS; i++)
-////		for (int j = 0; j < COLS; j++)
-////			arr2D[i][j] = rand() % 100;
-////}
-////
-//
-////void sortArr2D(int Arr2D[ROWS][COLS])
-////{
-////	for (int i = 0; i < ROWS; i++)
-////	{
-////		for (int j = 0; j < COLS; j++)
-////		{
-////			for (int k = i; k < ROWS; k++)
-////			{
-////				for (int l = k > i ? 0 : j + 1; l < COLS; l++)
-////				{
-////					if (Arr2D[k][l] < Arr2D[i][j])
-////						Arr2D[k][l] ^= Arr2D[i][j] ^= Arr2D[k][l] ^= Arr2D[i][j];
-////				}
-////			}
-////		}
-////	}
-////}
-
-#include <iostream>
-#include <cmath>
- 
+﻿#include<iostream>
 using namespace std;
-using namespace std::vector;
+
+#define tab "\t"
+const int Height = 5;
+const int Wight = 8;
+
+void randomArr2D(int arr2D[][Wight], const int Height, const int Wight);
+void randomArr2D(double Arr2D[][ptr], const int HeightD, const int ptr, int minRand, int maxRand);
+
+void printArr2D(int arr2D[][Wight], const int Height, const int Wight);
+void printArr2D(double Arr2D[][ptr], const int HeightD, const int ptr);
 
 
-void FullArr(int arr[][], const int Height, const int Wight)
-{
- 
-    for (int i = 0; i < Wight; i++)
-    {
-        for (int j = 0; j < Height; j++)
-        {
-            arr[i][j] = rand() % 10;
-        }
-    }
-}
- 
-void PrintArr(int arr[][], const int Height, const int Wight)
-{
-    for (int i = 0; i < Wight; i++)
-    {
-        for (int j = 0; j < Height; j++)
-        {
-            cout <<arr[i] [j];
-        }
-        cout << '\n';
-    }
-}
- 
- 
- 
 void main()
 {
-    const int Rows = 5, Cols = 5;
- 
-        int mass[Rows][Cols];
-        FullArr(mass, Rows,Cols);
-        PrintArr(mass, Rows, Cols);
-    system("pause");
+	setlocale(LC_ALL, "");
+	
+	int arr2D[Height][Wight];
+	
+	randomArr2D(arr2D, Height, Wight);
+	printArr2D(arr2D, Height, Wight);
+	
+	const int HeightD = 6;
+	const int WightD = 7;
+	int const* const ptr = &WightD;
+	double Arr2D[HeightD][ptr];
+	
+	randomArr2D(Arr2D, HeightD, ptr);
+	printArr2D(Arr2D, HeightD, ptr);
 }
+
+void randomArr2D(int arr2D[][Wight], const int Height, const int Wight)
+{
+	
+	for (int i = 0; i < Height; i++)
+		for (int j = 0; j < Wight; j++)
+		{
+			arr2D[i][j] = rand() % 100;
+		}
+}
+void printArr2D(int arr2D[][Wight], const int Height, const int Wight)
+{
+	for (int i = 0; i < Height; i++)
+	{
+		for (int j = 0; j < Wight; j++)
+		{
+			cout << arr2D[i][j] << tab;
+		}
+		cout << endl;
+	}
+}
+void randomArr2D(double Arr2D[][ptr], const int HeightD, const int ptr, int minRand, int maxRand)
+{
+
+	minRand *= 100;
+	maxRand *= 100;
+	for (int i = 0; i < HeightD; i++)
+	{
+		for (int j = 0; j < ptr; j++)
+		{
+			Arr2D[i][j] = rand() % (maxRand - minRand) + minRand;
+			Arr2D[i][j] /= 100;
+		}
+	}
+}
+
+void printArr2D(double Arr2D[][ptr], const int HeightD, const int ptr)
+{
+	for (int i = 0; i < Height; i++)
+	{
+		for (int j = 0; j < ptr; j++)
+		{
+			cout << Arr2D[i][j] << tab;
+		}
+		cout << endl;
+	}
+}
+
+//void sortArr2D(int Arr2D[ROWS][COLS])
+//{
+//	for (int i = 0; i < ROWS; i++)
+//	{
+//		for (int j = 0; j < COLS; j++)
+//		{
+//			for (int k = i; k < ROWS; k++)
+//			{
+//				for (int l = k > i ? 0 : j + 1; l < COLS; l++)
+//				{
+//					if (Arr2D[k][l] < Arr2D[i][j])
+//						Arr2D[k][l] ^= Arr2D[i][j] ^= Arr2D[k][l] ^= Arr2D[i][j];
+//				}
+//			}
+//		}
+//	}
+//}
